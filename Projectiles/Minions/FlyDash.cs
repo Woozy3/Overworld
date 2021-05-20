5061aed3fc557a5bc226a8fc3c58d3ec21af4e51
Stardust9681 <68380799+Stardust9681@users.noreply.github.com>
2021-05-20 16:11:40 -0500

    Changed FlyDash a bit (see description)
    Still works with previous variables ("backwards compatible" with old child classes, unless they used OnCreated() hook), but has more options now.
    - Can change how soon the projectile can perform its dash attack when it's spawned
    - Can string multiple dashes together before going on cooldown
    - Can tell the projectile to die when it doesn't have a target
    - Targets closest NPC instead of first in Main.npc within range.
    - Doesn't throw a fit when none of the values are assigned, because there are default values now.

--- Projectiles/Minions/FlyDash.cs
+++ Projectiles/Minions/FlyDash.cs
@@ -6,22 +6,24 @@ namespace Overworld.Projectiles.Minions
 {
 	public abstract class DashFly : Minion
 	{
-		//Anything labelled "PROTECTED" needs to be set in SetDefaults() of the projectile. This is most of the following.
-		protected float accellerationMult; //how quickly it accellerates, or something.
-		protected float maxSpeed; //maximum speed normally
-		protected float dashSpeed; //maximum dashing speed
-		protected float dashTime; //Cooldown between dashes.
+		//Anything labelled "PROTECTED" should be set in SetDefaults() of the projectile. This is most of the following.
+		protected float accellerationMult = 0f; //how quickly it accellerates, or something.
+		protected float maxSpeed = 12f; //maximum speed normally
+		protected float dashSpeed = 20f; //maximum dashing speed
+		protected float dashTime = 180f; //Cooldown between dashes.
 
-		protected float detectionRadius; //how far the minion can see
-		protected bool canSeeThroughTiles; //Can the minion see through tiles?
-		protected float followDistance; //how far will the minion go before returning to player?
-		protected float hoverRadius; //how far the projectile will passively float around the target
+		protected float detectionRadius = 300f; //how far the minion can see
+		protected bool canSeeThroughTiles = true; //Can the minion see through tiles?
+		protected float followDistance = 600f; //how far will the minion go before returning to player?
+		protected float hoverRadius = 160f; //how far the projectile will passively float around the target
+		protected bool killWithoutTarget = false; //Kill the projectile when it doesn't have a target?
 
-		protected float dashLength; //how far away the minion can dash
+		protected float dashLength = 12f; //how far away the minion can dash
 		private float dashCounter; //counts dash length in travel, used to modify dash speed slightly
 
 		private Vector2 targetPos;
 		private int AiState; //0 = resting, 1 = attacking, 2 = dashing
+		private int dashes = 0; //How many times it has consecutively dashed.
 
 		#region Timers
 		public float DashCooldown //Just the dash timer / cooldown. Fancy way of getting projectile.ai[0]
@@ -32,7 +34,7 @@ namespace Overworld.Projectiles.Minions
 		#endregion
 		public override void Behavior()
 		{
-			bool defaultRotation = ModifyRotation(AiState); //The way this is called ensures that it does not actually interfere with the default AI.
+			bool defaultRotation = ModifyRotation(AiState);
 			Player player = Main.player[projectile.owner]; //projectile's owner.
 			bool hasTarget = false;
 			targetPos = player.position; //Vector2 to aim for
@@ -40,11 +42,13 @@ namespace Overworld.Projectiles.Minions
 			{
 				if (!hasTarget) //Will always run, but I don't really care.
 				{
+					float distance = detectionRadius * 2f;
 					for (int i = 0; i < Main.maxNPCs; i++) //iterate through Main.npc[]
 					{
 						NPC target = Main.npc[i]; //get each target
-						if (Vector2.Distance(projectile.position, target.position) <= detectionRadius && !target.friendly && target.type != NPCID.TargetDummy && target.lifeMax > 10 && target.active && target.life > 0)
+						if (Vector2.Distance(projectile.position, target.position) <= detectionRadius && Vector2.Distance(projectile.position, target.position) < distance && !target.friendly && target.type != NPCID.TargetDummy && target.lifeMax > 10 && target.active && target.life > 0)
 						{
+							distance = projectile.Distance(target.position);
 							//Make a ton of comparisons. In essence, target is in range, not dead, not friendly, has more hp than a worm, and isn't a target dummy
 							if (!canSeeThroughTiles) //if the projectile *can't* "see" through tiles
 							{
@@ -52,17 +56,12 @@ namespace Overworld.Projectiles.Minions
 								{
 									hasTarget = true; //has a target
 									targetPos = target.position; //has coords
-									projectile.velocity = Vector2.Zero; //reset velocity, this is because we add onto the velocity to move towards the target
-									break; //ends for and while loops.
-									//Anything here would not run because of break;
 								}
 							}
 							else //if the projectile *can* "see" through tiles
 							{
 								hasTarget = true; //see above
 								targetPos = target.position;
-								projectile.velocity = Vector2.Zero;
-								break;
 							}
 						}
 					}
@@ -70,24 +69,32 @@ namespace Overworld.Projectiles.Minions
 
 				if (hasTarget) //ai change
 				{
+					projectile.velocity = Vector2.Zero;
 					AiState = 1; //attacking
 				}
 				else if (!hasTarget) //ai change
 				{
-					AiState = 0; //idle
+					if (killWithoutTarget)
+					{
+						projectile.timeLeft = 2;
+						projectile.active = false;
+						projectile.Kill();
+					}
+					else
+						AiState = 0; //idle
 				}
 
 				if (AiState == 0) //idle
 				{
-					if(defaultRotation) //If the minion doesn't handle rotation
-						projectile.rotation = 0; //set rotation to 0
-					if(projectile.Distance(player.position) <= followDistance && projectile.Distance(player.position) > 128f) //if the projectile is a distance away from the player
+					if (defaultRotation)
+						projectile.rotation = 0;
+					if (projectile.Distance(player.position) <= followDistance && projectile.Distance(player.position) > 128f) //if the projectile is a distance away from the player
 					{
-						projectile.velocity = (projectile.DirectionTo(player.position) * maxSpeed) * (1 + accellerationMult*0.6f); //change the velocity towards the player
+						projectile.velocity = (projectile.DirectionTo(player.position) * maxSpeed) * (1 + accellerationMult * 0.6f); //change the velocity towards the player
 					}
-					else if(projectile.Distance(player.position) > followDistance) //if the projectile is further away from the player
+					else if (projectile.Distance(player.position) > followDistance) //if the projectile is further away from the player
 					{
-						projectile.velocity = (projectile.DirectionTo(player.position) * maxSpeed) * (1 + accellerationMult*1.4f); //change velocity towards the player, but faster
+						projectile.velocity = (projectile.DirectionTo(player.position) * maxSpeed) * (1 + accellerationMult * 1.4f); //change velocity towards the player, but faster
 					}
 					if (projectile.velocity == Vector2.Zero) //just so there's no non-moving projectiles.
 						projectile.velocity = Vector2.One; //set it to (1,1)
@@ -96,15 +103,15 @@ namespace Overworld.Projectiles.Minions
 				{
 					if (projectile.velocity == Vector2.Zero) //just so there's no non-moving projectiles.
 						projectile.velocity.Y = -1f; //set y-vel to -1
-					if(defaultRotation) //If the minion doesn't handle rotation
-						projectile.rotation = 0; //set rotation to 0
-					if (Vector2.Distance(projectile.position, targetPos) > hoverRadius*0.9f) //if the distance is greater than the hoverRadius
+					if (defaultRotation)
+						projectile.rotation = 0;
+					if (Vector2.Distance(projectile.position, targetPos) > hoverRadius * 0.9f) //if the distance is greater than the hoverRadius
 					{
 						projectile.velocity += (Vector2.Normalize(targetPos - projectile.position) * maxSpeed) * (1 + accellerationMult); //add velocity towards the target position
 						MathHelper.Clamp(projectile.velocity.X, -maxSpeed, maxSpeed); //max x vel
 						MathHelper.Clamp(projectile.velocity.Y, -maxSpeed, maxSpeed); //max y vel
 					}
-					if (Vector2.Distance(projectile.position, targetPos) <= hoverRadius*1.1f) //if distance is less than hoverRadius
+					if (Vector2.Distance(projectile.position, targetPos) <= hoverRadius * 1.1f) //if distance is less than hoverRadius
 					{
 						if (DashCooldown >= dashTime) //check to see if dash is off cooldown.
 						{
@@ -118,34 +125,73 @@ namespace Overworld.Projectiles.Minions
 						MathHelper.Clamp(projectile.velocity.Y, -maxSpeed, maxSpeed); //max y vel
 					}
 				}
-				if(projectile.Distance(player.position) >= followDistance+detectionRadius*2f) //if this gets too far from the player...
+				if (projectile.Distance(player.position) >= followDistance + detectionRadius * 2f) //if this gets too far from the player...
 				{
 					projectile.position = player.Center; //teleport back
 					AiState = 0; //set the ai to idle
 				}
 			}
-			if(AiState == 2) //during the dash
+			if (AiState == 2) //during the dash
 			{
 				if (dashCounter > dashLength * 0.95f) //during the start
-					projectile.velocity *= (1+accellerationMult); //slight accelleration for a whole almost no time lol.
+					projectile.velocity *= (1 + accellerationMult); //slight accelleration for a whole almost no time lol.
 				if (dashCounter < dashLength * 0.05f) //during the end
-					projectile.velocity *= (1-accellerationMult); //slight deccelleration for a whole almost no time xD.
+					projectile.velocity *= (1 - accellerationMult); //slight deccelleration for a whole almost no time xD.
 				if (dashCounter <= 0f) //when the counter runs out (<= 0)
+				{
 					AiState = 1; //set ai state to attacking
+					float temp = 0f;
+					PostDash(dashes+1);
+					temp = ModifyPostDash(dashes++);
+					DashCooldown = dashTime - temp;
+					projectile.ai[1] = dashTime;
+					if (DashCooldown >= dashTime)
+						dashes++;
+					else
+						dashes = 0;
+				}
 				dashCounter -= 1f; //subtract from dash counter, so it can properly reset ai state
-				if(defaultRotation) //if the minion doesn't handle rotation
-					projectile.rotation = projectile.velocity.ToRotation()+MathHelper.PiOver2; //make the sprite this rotation.
-				//IMPORTANT: Sprite must be facing UPWARDS for this to work. Else, you must write your own rotation code (and return false in hook)
+				if (defaultRotation)
+					projectile.rotation = projectile.velocity.ToRotation() + MathHelper.PiOver2; //rotation. Assumes projectile is facing upwards
 			}
-			if(DashCooldown < dashTime * 1.5f) //ensures not to reach or surpass float.maxValue
+			if (DashCooldown < dashTime * 1.5f) //ensures not to reach or surpass float.maxValue
 				DashCooldown += 1f; //decrease cooldown (I know it's backwards, shut up)
 		}
-		
+		public override void OnCreated()
+		{
+				SafeOnCreated();
+				DashCooldown = dashTime - ModifyCooldown(DashCooldown);
+		}
+
+		/// <summary>
+		/// Called when the projectile is created.
+		/// </summary>
+		public virtual void SafeOnCreated() { }
+
+		/// <summary>
+		/// Called when the projectile is created to modify the time before next dash. 0 = immediately.
+		/// </summary>
+		/// <param name="dashCooldown"></param>
+		public virtual float ModifyCooldown(float dashCooldown) { return dashCooldown; }
+
 		/// <summary>
 		/// Called before AI. Return false to handle rotation manually. Returns true by default.
 		/// </summary>
 		/// <param name="currentAI">0 = idle, 1 = found enemy, 2 = dashing</param>
 		public virtual bool ModifyRotation(int currentAI) { return true; }
+
+		/// <summary>
+		/// Called after dashing. Might use this for something idk.
+		/// </summary>
+		/// <param name="consecutiveDashes">Number of dashes consecutively</param>
+		public virtual void PostDash(int consecutiveDashes) { }
+
+		/// <summary>
+		/// Allows modification of the dash cooldown after dashing. Can be used to string multiple together. Returns max cooldown by default. 0 is no time before next attack
+		/// </summary>
+		/// <param name="consecutiveDashes">Number of consecutive dash attacks</param>
+		/// <returns></returns>
+		public virtual float ModifyPostDash(int consecutiveDashes) { return dashTime; }
 
 		/// <summary>
 		/// Returns the location the projectile is aiming for.
