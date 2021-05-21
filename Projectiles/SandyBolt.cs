09883bb1ee4dd619d784b5d70fbea6f7474b6741
Pedguin12 <71602140+Pedguin12@users.noreply.github.com>
2021-05-21 14:02:09 -0600

    funny sand projectile

---
+++ Projectiles/SandyBolt.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace WhisperingDeath.Projectiles.Bosses
+{
+    public class SandyBolt : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sand Shard");
+        }
+
+        public override void SetDefaults()
+        {
+
+            projectile.width = 12;
+            projectile.height = 12;
+            projectile.damage = 23;
+            projectile.aiStyle = 0;
+            projectile.penetrate = 3;
+            projectile.friendly = false;
+            projectile.hostile = true;
+            projectile.tileCollide = true;
+            projectile.timeLeft = 300;
+        }
+        public override void AI()
+        {
+            Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 32, projectile.velocity.X * -0.5f, projectile.velocity.Y * -0.5f);   //spawns dust behind it, this is a spectral light blue dust
+        }
+
+    }
+}
