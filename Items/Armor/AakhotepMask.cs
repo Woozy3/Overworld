70330c3ec8f164f51a96d0a39fa5daebd2915fff
Pedguin12 <71602140+Pedguin12@users.noreply.github.com>
2021-05-21 13:56:44 -0600

    Add files via upload

---
+++ Items/Armor/AakhotepMask.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace WhisperingDeath.Items.Boss
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class AakhotepMask : ModItem
+	{
+        public override void SetDefaults()
+		{
+			item.width = 28;
+			item.height = 20;
+			item.rare = ItemRarityID.Orange;
+			item.vanity = true;
+		}
+
+		public override bool DrawHead()
+		{
+			return false;
+		}
+	}
+}
